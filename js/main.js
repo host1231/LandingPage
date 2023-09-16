d342006d644b18a40d6880951e1f2f9f7092c85e
User <pg15182100@gmail.com>
2023-09-16 22:30:25 +0400

    Second part of the project

--- js/main.js
+++ js/main.js
@@ -22,4 +22,29 @@ document.querySelector('.header__btn').addEventListener('click', ()=>{
             document.querySelector('.header__top').classList.remove('open');
         })
     });
-})
+});
+
+
+window.addEventListener('load', () => {
+    const preloader = document.querySelector('.preloader');
+    preloader.classList.add('hide');
+    setTimeout(() => {
+        preloader.remove();
+    }, 500);
+    
+    const effectAnim = () => {
+        const effect = document.querySelectorAll('.effect')
+        const windowHeight = window.scrollY + window.innerHeight;
+        effect.forEach(el => {
+            const elHeight = el.offsetHeight  + el.offsetTop - 100;
+            if (windowHeight >= elHeight) {
+                el.classList.add('anim')
+            }
+        })
+        
+    }
+
+    window.addEventListener('scroll', () => {
+        effectAnim();
+    });
+});
